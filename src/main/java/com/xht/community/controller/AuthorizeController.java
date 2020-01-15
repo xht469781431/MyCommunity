a75211ddb405477b06f3e1672f3a0f3beb7e582d
xht469781431 <469781431@qq.com>
2020-01-15 16:15:32 +0800

    将一些信息写入到配置文件中

--- src/main/java/com/xht/community/controller/AuthorizeController.java
+++ src/main/java/com/xht/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.xht.community.dto.AccessTokenDTO;
 import com.xht.community.dto.GithubUser;
 import com.xht.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,18 +15,22 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirectUri}")
+    private String redirectUri;
+
     //授权的回调路径
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state){
 
         //创建一个Access对象作为参数传递
-        AccessTokenDTO accessTokenDTO = new AccessTokenDTO(
-                "6273d47212d9111ed940",
-                "3f139c7b4186462f4fa78deef6194600da6b7f0f",
-                code,
-                "http://localhost:8887/callback",
-                state);
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO(clientId,clientSecret,code,redirectUri,state);
 
         //调用方法得到accessToken字符串 用来获取User信息
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
